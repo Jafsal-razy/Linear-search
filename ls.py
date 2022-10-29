3bc7be2a8f097cbe9d1411c99712ca4ada455996
ameen0222 <91777238+ameen0222@users.noreply.github.com>
2022-10-29 23:43:10 +0530

    Update ls.py

--- ls.py
+++ ls.py
@@ -6,18 +6,19 @@ def search(arr, N, x):
             return i
     return -1
  
- 
+#udayipp T-shirt function
 # Driver Code below
 if __name__ == "__main__":
-    arr = [2, 3, 4, 10, 40, 60, 152]
+    arr = [2, 3, 4, 10, 40, 60, 152, 200, 450]
     x = 10
     N = len(arr)
  
     # Function call
     result = search(arr, N, x)
     if(result == -1):
-        print("Element is not present in array : ")
+        print("Element is not present in array :  ")
+      
     else:
-        print("Element is present at index :", result)
+        print("Element is present at index : ", result)
 
-      # End
+      # End Now
